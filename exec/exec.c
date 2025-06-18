25274814a16fefcac1cb7071688883c72f81f5bf
Oussama Elhassouni <oelhasso@c1r7p5.1337.ma>
2025-06-18 22:55:47 +0100

    savvvv

--- exec/exec.c
+++ exec/exec.c
@@ -6,36 +6,65 @@
 /*   By: oelhasso <oelhasso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 17:26:38 by oelhasso          #+#    #+#             */
-/*   Updated: 2025/06/17 22:09:29 by oelhasso         ###   ########.fr       */
+/*   Updated: 2025/06/18 21:24:56 by oelhasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+#include "exec.h"
 
-void test(t_cmd *cmd)
+// void test(t_cmd *cmd)
+// {
+// 	int i;
+
+// 	i = 0;
+// 	while (cmd)
+// 	{
+// 		printf ("1---\n");
+// 		i = 0;
+// 		while (cmd->commands[i])
+// 			printf ("cmd : %s\n", cmd->commands[i++]);
+// 		while (cmd->red)
+// 		{
+// 			printf ("file : %s\n", cmd->red->file);
+// 			printf ("type : %u\n", cmd->red->red_type);
+// 			cmd->red = cmd->red->next;
+// 		}
+// 		cmd = cmd->next;
+// 		printf ("2---\n");
+// 	}
+// }
+
+int is_pipe(t_cmd *cmd, t_info *dainfo)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (cmd)
 	{
-		printf ("1---\n");
-		i = 0;
-		while (cmd->commands[i])
-			printf ("cmd : %s\n", cmd->commands[i++]);
-		while (cmd->red)
-		{
-			printf ("file : %s\n", cmd->red->file);
-			printf ("type : %u\n", cmd->red->red_type);
-			cmd->red = cmd->red->next;
-		}
+		i++;
 		cmd = cmd->next;
-		printf ("2---\n");
 	}
+	if (i > 1)
+		dainfo->a_pipe = 1;
+	else
+		dainfo->a_pipe = 0;
+	return 0;
 }
 
 int exec (t_cmd *cmd, t_env *env)
 {
-	printf ("env : %s\n", env->key);
-	return 0;
+	t_info *dainfo;
+	// t_env *tmp;
+	// tmp = env;
+
+	// test(cmd);
+	is_pipe(cmd, dainfo);
+	while (cmd)
+	{
+		dupping();
+		
+		cmd = cmd->commands;
+	}
+	return (0);
 }
--- exec/exec.h
+++ exec/exec.h
@@ -6,14 +6,19 @@
 /*   By: oelhasso <oelhasso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 17:28:22 by oelhasso          #+#    #+#             */
-/*   Updated: 2025/06/17 17:31:46 by oelhasso         ###   ########.fr       */
+/*   Updated: 2025/06/18 20:46:49 by oelhasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_H
 # define EXEC_H
 
+
 #include "../minishell.h"
+
+typedef struct s_info {
+    int a_pipe;
+}	t_info;
 
 int exec (t_cmd *cmd, t_env *env);
 
